c5f7c76bf7723a26a6be9de87f97e7be348eeda1
My Name <masencevm06@gmail.com>
2025-06-08 00:11:15 +0300

    Initial commit with economic formulas calculator

--- components/FormulaLatex.tsx
+++ components/FormulaLatex.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { BlockMath } from "react-katex";
 import "katex/dist/katex.min.css";
+import styles from "../app/page.module.css";
 
 export default function FormulaLatex({ formula }: { formula: string }) {
-  return <BlockMath math={formula} />;
+  return <div className={styles.katexWrapper}><BlockMath math={formula} /></div>;
 }
